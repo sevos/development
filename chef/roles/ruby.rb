92a78ccd0a84ac76582ba094128e527196453e04
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-05-05 20:51:38 +0200

    initial commit

---
+++ chef/roles/ruby.rb
@@ -0,0 +1,16 @@
+name "ruby"
+description "installs rvm and given ruby version for vagrant user"
+
+run_list "recipe[rvm::user]"
+
+default_attributes(
+  'rvm' => {
+    'user_installs' => [
+      {
+        'user' => 'vagrant',
+        'default_ruby' => '1.9.2'
+      }
+    ]
+  }
+)
+
