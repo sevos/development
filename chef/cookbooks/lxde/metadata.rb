1ba011be125656e761d7c611ebb53f64edac5a8e
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-05-12 23:15:26 +0200

    Add lxde recipe

---
+++ chef/cookbooks/lxde/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Artur Roszczyk"
+maintainer_email "artur.roszczyk@gmail.com  "
+license          "Apache 2.0"
+description      "Installs lxde"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
