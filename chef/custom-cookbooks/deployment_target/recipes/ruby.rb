1dbd5319ed437b9efd905954ad59b944018da776
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-11-06 11:59:18 +0100

    Nginx on ruby deployment target

--- chef/custom-cookbooks/deployment_target/recipes/ruby.rb
+++ chef/custom-cookbooks/deployment_target/recipes/ruby.rb
@@ -31,3 +31,14 @@ template '/home/deploy/shared/config/database.yml' do
   variables(:environment => node[:application][:environment],
             :database => node[:application][:database])
 end
+
+include_recipe 'nginx::source', 'nginx::http_gzip_static_module'
+
+template "/etc/nginx/sites-enabled/000-default" do
+  source 'nginx-vhost.erb'
+  owner "www-data"
+  group "users"
+  mode "0644"
+  notifies :reload, "service[nginx]"
+end
+
